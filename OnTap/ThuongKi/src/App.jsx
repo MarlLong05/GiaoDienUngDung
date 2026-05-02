dbd3b567d621f7e04bce84c6d692152d4f3eb58d
Thanh Long <longffwar@gmail.com>
2026-05-02 23:24:27 +0700

    Update Router

--- OnTap/ThuongKi/src/App.jsx
+++ OnTap/ThuongKi/src/App.jsx
@@ -1,13 +1,36 @@
-import FetchAPI from './components/FetchAPI'
-import  Loading  from './components/Loading'
-import  InputFetch  from './components/InputFetch'
+
+import {Routes, Route} from "react-router-dom"
+import { Link } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
+import News from './components/News'
+import Contact from './components/Contact'
+
 function App() {
   return (
     <>
-      {/* <FetchAPI/> */}
-     {/* <Loading /> */}
-     <InputFetch />
+  
+    <nav>
+      <ul>
+        <li>
+          <Link to ="/"> Home</Link>
+        </li>
+        <li>
+          <Link to="/News"> News</Link>
+        </li>
+        <li>
+          <Link to="/Contact"> Contact</Link>
+        </li>
+      </ul>
+    </nav>
+
+      <Routes>
+      <Route path='/' element={<Home /> }> </Route>
+      <Route path="/News" element={<News/>}>  </Route>
+      <Route path='/Contact' element={<Contact/>}></Route>
+    </Routes>
+
+   
     </>
   )
 }
